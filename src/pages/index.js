d72b9dae263e7b08b0a6ffc74dedf9b9537a0dac
tsuchinoko0402 <okazaki@zakioka.net>
2021-09-25 15:38:03 +0900

    [add]SDGs アイコン追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Layout from '../components/Layout';
 
+import wheel from '../assets/images/sdgs_icon/wheel.png' 
 import pic1 from '../assets/images/01.jpg';
 import pic2 from '../assets/images/02.jpg';
 import pic3 from '../assets/images/03.jpg';
@@ -17,186 +18,251 @@ const IndexPage = () => (
       <div className="inner">
         <header>
           <h1>
-            This is Phantom, a free, fully responsive gatsby starter
+            ボーイスカウト阪神さくら地区　活動振興大会2021　特設ページ
             <br />
           </h1>
           <p>
-            Etiam quis viverra lorem, in semper lorem. Sed nisl arcu euismod sit
-            amet nisi euismod sed cursus arcu elementum ipsum arcu vivamus quis
-            venenatis orci lorem ipsum et magna feugiat veroeros aliquam. Lorem
-            ipsum dolor sit amet nullam dolore.
+            本年の活動テーマは<b>「みんなでもっと知ろうSDG's」</b>です。 SDG's の 17 の活動目標について、
+            各団が一つずつ研究しました。 <br />
+            本来であれば、夙川河川敷の各所に展示する予定だった、各団の研究成果を掲載します。
           </p>
         </header>
         <section className="tiles">
           <article className="style1">
             <span className="image">
-              <img src={pic1} alt="" />
+              <img src={wheel} alt="" />
             </span>
             <Link to="/Generic">
-              <h2>Magna</h2>
+              <h2>1 貧困をなくそう</h2>
               <div className="content">
                 <p>
-                  Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros
-                  et feugiat.
+                  西宮第2団
                 </p>
               </div>
             </Link>
           </article>
           <article className="style2">
             <span className="image">
-              <img src={pic2} alt="" />
+              <img src={wheel} alt="" />
             </span>
             <Link to="/Generic">
-              <h2>Lorem</h2>
+              <h2>2 飢餓をゼロに</h2>
               <div className="content">
                 <p>
-                  Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros
-                  et feugiat.
+                  西宮第9団
                 </p>
               </div>
             </Link>
           </article>
           <article className="style3">
             <span className="image">
-              <img src={pic3} alt="" />
+              <img src={wheel} alt="" />
             </span>
             <Link to="/Generic">
-              <h2>Feugiat</h2>
+              <h2>3 すべての人に健康と福祉を</h2>
               <div className="content">
                 <p>
-                  Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros
-                  et feugiat.
+                  西宮第10団
                 </p>
               </div>
             </Link>
           </article>
           <article className="style4">
             <span className="image">
-              <img src={pic4} alt="" />
+              <img src={wheel} alt="" />
             </span>
             <Link to="/Generic">
-              <h2>Tempus</h2>
+              <h2>4 質の高い教育をみんなに</h2>
               <div className="content">
                 <p>
-                  Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros
-                  et feugiat.
+                  芦屋第1団
                 </p>
               </div>
             </Link>
           </article>
           <article className="style5">
             <span className="image">
-              <img src={pic5} alt="" />
+              <img src={wheel} alt="" />
             </span>
             <Link to="/Generic">
-              <h2>Aliquam</h2>
+              <h2>5 ジェンダー平等を実現しよう</h2>
               <div className="content">
                 <p>
-                  Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros
-                  et feugiat.
+                  西宮第27団
                 </p>
               </div>
             </Link>
           </article>
           <article className="style6">
             <span className="image">
-              <img src={pic6} alt="" />
+              <img src={wheel} alt="" />
             </span>
             <Link to="/Generic">
-              <h2>Veroeros</h2>
+              <h2>6 安全な水とトイレを世界中に</h2>
               <div className="content">
                 <p>
-                  Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros
-                  et feugiat.
+                  西宮第22団
                 </p>
               </div>
             </Link>
           </article>
-          <article className="style2">
+          <article className="style7">
             <span className="image">
-              <img src={pic1} alt="" />
+              <img src={wheel} alt="" />
             </span>
             <Link to="/Generic">
-              <h2>Ipsum</h2>
+              <h2>7 エネルギーをみんなにそしてクリーンに</h2>
               <div className="content">
                 <p>
-                  Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros
-                  et feugiat.
+                  西宮第1団
                 </p>
               </div>
             </Link>
           </article>
-          <article className="style3">
+          <article className="style8">
             <span className="image">
-              <img src={pic2} alt="" />
+              <img src={wheel} alt="" />
             </span>
             <Link to="/Generic">
-              <h2>Dolor</h2>
+              <h2>8 働きがいも経済成長も</h2>
               <div className="content">
                 <p>
-                  Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros
-                  et feugiat.
+                  西宮第3団
                 </p>
               </div>
             </Link>
           </article>
-          <article className="style1">
+          <article className="style9">
             <span className="image">
-              <img src={pic3} alt="" />
+              <img src={wheel} alt="" />
             </span>
             <Link to="/Generic">
-              <h2>Nullam</h2>
+              <h2>9 産業と技術革新の基盤をつくろう</h2>
               <div className="content">
                 <p>
-                  Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros
-                  et feugiat.
+                  西宮第6団
                 </p>
               </div>
             </Link>
           </article>
-          <article className="style5">
+          <article className="style10">
             <span className="image">
-              <img src={pic4} alt="" />
+              <img src={wheel} alt="" />
             </span>
             <Link to="/Generic">
-              <h2>Ultricies</h2>
+              <h2>10 人や国の不平等をなくそう</h2>
               <div className="content">
                 <p>
-                  Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros
-                  et feugiat.
+                  西宮第20団
                 </p>
               </div>
             </Link>
           </article>
-          <article className="style6">
+          <article className="style11">
             <span className="image">
-              <img src={pic5} alt="" />
+              <img src={wheel} alt="" />
             </span>
             <Link to="/Generic">
-              <h2>Dictum</h2>
+              <h2>11 住み続けられるまちづくりを</h2>
               <div className="content">
                 <p>
-                  Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros
-                  et feugiat.
+                  西宮第11団
                 </p>
               </div>
             </Link>
           </article>
-          <article className="style4">
+          <article className="style12">
             <span className="image">
-              <img src={pic6} alt="" />
+              <img src={wheel} alt="" />
             </span>
             <Link to="/Generic">
-              <h2>Pretium</h2>
+              <h2>12 つくる責任つかう責任</h2>
               <div className="content">
                 <p>
-                  Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros
-                  et feugiat.
+                  西宮第21団
                 </p>
               </div>
             </Link>
           </article>
-        </section>
+          <article className="style13">
+            <span className="image">
+              <img src={wheel} alt="" />
+            </span>
+            <Link to="/Generic">
+              <h2>13 気候変動に具体的な対策を</h2>
+              <div className="content">
+                <p>
+                  西宮第13団
+                </p>
+              </div>
+            </Link>
+          </article>
+          <article className="style14">
+            <span className="image">
+              <img src={wheel} alt="" />
+            </span>
+            <Link to="/Generic">
+              <h2>14 海の豊かさを守ろう</h2>
+              <div className="content">
+                <p>
+                  西宮第12団
+                </p>
+              </div>
+            </Link>
+          </article>
+          <article className="style15">
+            <span className="image">
+              <img src={wheel} alt="" />
+            </span>
+            <Link to="/Generic">
+              <h2>15 陸の豊かさも守ろう</h2>
+              <div className="content">
+                <p>
+                  西宮第7団
+                </p>
+              </div>
+            </Link>
+          </article>
+          <article className="style16">
+            <span className="image">
+              <img src={wheel} alt="" />
+            </span>
+            <Link to="/Generic">
+              <h2>16 平和と公正をすべての人に</h2>
+              <div className="content">
+                <p>
+                  西宮第25団
+                </p>
+              </div>
+            </Link>
+          </article>
+          <article className="style17">
+            <span className="image">
+              <img src={wheel} alt="" />
+            </span>
+            <Link to="/Generic">
+              <h2>17 パートナーシップで目標を達成しよう</h2>
+              <div className="content">
+                <p>
+                  芦屋第3団
+                </p>
+              </div>
+            </Link>
+          </article>
+          <article className="style18">
+            <span className="image">
+              <img src={wheel} alt="" />
+            </span>
+            <Link to="/Generic">
+              <h2>SDGs とは？</h2>
+              <div className="content">
+                <p>
+                  
+                </p>
+              </div>
+            </Link>
+          </article>
+       </section>
       </div>
     </div>
   </Layout>
