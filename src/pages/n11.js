c24089356eb32341d7834959ff7b4e0262871dfd
tsuchinoko0402 <okazaki@zakioka.net>
2021-09-26 16:33:54 +0900

    [add]N11の写真追加

--- src/pages/n11.js
+++ src/pages/n11.js
@@ -3,6 +3,9 @@ import React from 'react';
 import Layout from '../components/Layout';
 import icon from '../assets/images/sdgs_icon/11.png';
 import image1 from '../assets/images/n11_bvs.jpg';
+import image2 from '../assets/images/n11_cs_all.jpg';
+import image3 from '../assets/images/n11_cs_1.jpg';
+import image4 from '../assets/images/n11_cs_2.jpg';
 
 const IndexPage = () => (
   <Layout>
@@ -15,6 +18,15 @@ const IndexPage = () => (
         <span className="image main">
           <img src={image1} alt="" />
         </span>
+        <span className="image main">
+          <img src={image2} alt="" />
+        </span>
+        <div className="box alt">
+						<div className="row gtr-uniform">
+              <div className="col-6"><span className="image fit"><img src={image3} alt="" /></span></div>
+              <div className="col-6"><span className="image fit"><img src={image4} alt="" /></span></div>
+          </div>
+        </div>
         <span className="image sdgs_icon">
           <img src={icon} alt="" />
         </span>
