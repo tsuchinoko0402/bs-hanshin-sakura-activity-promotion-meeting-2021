06373afddc7246f2ddf6cf40b6067a577f04ba79
tsuchinoko0402 <okazaki@zakioka.net>
2021-10-13 00:23:09 +0900

    [add]N22 追加

--- src/pages/n22.js
+++ src/pages/n22.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Layout from '../components/Layout';
 import icon from '../assets/images/sdgs_icon/06.png';
+import image1 from '../assets/images/n22/n22_cs_1.jpg';
+import image2 from '../assets/images/n22/n22_cs_2.jpg';
 
 const IndexPage = () => (
   <Layout>
@@ -11,6 +13,12 @@ const IndexPage = () => (
         <p>
           担当：西宮第22団
         </p>
+        <span className="image main">
+          <img src={image1} alt="" />
+        </span>
+        <span className="image main">
+          <img src={image2} alt="" />
+        </span>
         <span className="image sdgs_icon">
           <img src={icon} alt="" />
         </span>
